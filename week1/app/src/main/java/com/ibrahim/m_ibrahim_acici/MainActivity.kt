3306fbf92ba872591862a87de9cc1a53ba7eaad1
MibrahimAcici <ibrahimacici@gmail.com>
2022-08-24 16:33:59 +0300

    RollDice

---
+++ week1/app/src/main/java/com/ibrahim/m_ibrahim_acici/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.ibrahim.m_ibrahim_acici
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var button: Button
+    private lateinit var imageView: ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button = findViewById(R.id.button)
+        imageView = findViewById(R.id.imageView)
+
+        button.setOnClickListener {
+            rollDice()
+        }
+    }
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6)+1
+        val drawableResource = when(randomInt){
+            1 -> R.drawable.ic_dice_1
+            2 -> R.drawable.ic_dice_2
+            3 -> R.drawable.ic_dice_3
+            4 -> R.drawable.ic_dice_4
+            5 -> R.drawable.ic_dice_5
+            else -> R.drawable.ic_dice_6
+        }
+        imageView.setImageResource(drawableResource)
+    }
+}
